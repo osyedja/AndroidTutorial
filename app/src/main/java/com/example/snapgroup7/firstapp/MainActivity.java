2aed75f2ccb3ec0f3f46fb0f5e8a6b21694b4f4c
osyed <osyed@snapgroup.co>
2018-08-22 17:59:16 +0300

    game

--- app/src/main/java/com/example/snapgroup7/firstapp/MainActivity.java
+++ app/src/main/java/com/example/snapgroup7/firstapp/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.snapgroup7.firstapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+
+
 }
